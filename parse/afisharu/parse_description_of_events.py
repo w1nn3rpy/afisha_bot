e44909d4ca054d8aacc08f46d61042877ffc62ce
w1nn3rpy <l33sa1337@gmail.com>
2025-03-08 14:25:29 +0400

    fix

--- parse/afisharu/parse_description_of_events.py
+++ parse/afisharu/parse_description_of_events.py
@@ -1,7 +1,10 @@
+import asyncio
+import random
 import re
 import time
 import shutil
 import multiprocessing
+import traceback
 from typing import List, Dict
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -11,56 +14,95 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from config import logger
+from database.events_db import delete_event_by_url
 from parse.afisharu.parse_events import init_driver
 
 
-def get_event_description(url: str) -> Dict[str, str]:
+def get_event_description(process_id, list_of_links: List[str]) -> Dict[str, str] | None:
     """Получает описание мероприятия по ссылке."""
+
+    descriptions = {url: 'Нет описания' for url in list_of_links}
+
+
+    all_count = len(list_of_links)
+    current_count = 0
+
     driver = init_driver()
-    description = "Нет описания"
     try:
-        logger.info(f"Открываем страницу: {url}")
-        driver.get(url)
-        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
-        logger.info("Страница загружена!")
+        for url, description in descriptions.items():
+            attempts = 0
+            max_attempts = 5
 
-        try:
-            error_element = driver.find_element(By.CSS_SELECTOR, "h1.error-page__title")
-            if "Данная страница не найдена!" in error_element.text:
-                logger.warning(f"Страница 404! Пропускаем {url}")
-                return {url: description}
-        except:
-            pass  # Ошибки нет, продолжаем
+            while attempts < max_attempts:
 
-        try:
-            # Ждём появления блока описания
-            description_block = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-test='OBJECT-DESCRIPTION-CONTENT']"))
-            )
-            soup = BeautifulSoup(description_block.get_attribute("innerHTML"), "html.parser")
+                logger.info(f"[{process_id}] [INFO] {current_count}/{all_count} Открываем страницу: {url}")
+                driver.get(url)
+                WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+                logger.info(f"[{process_id}] [INFO] Страница загружена!")
 
-            # Пробуем найти основной текст внутри RESTRICT-TEXT
-            first_paragraph = soup.find("div", {"data-test": "RESTRICT-TEXT"})
+                try:
+                    error_element = driver.find_element(By.CSS_SELECTOR, "h1.error-page__title")
+                    if "Данная страница не найдена!" in error_element.text:
+                        logger.warning(f"[{process_id}] ⚠️ Страница 404! Удаляем {url}")
+                        asyncio.run(delete_event_by_url(url))
+                        break
+                except:
+                    pass  # Ошибки нет, продолжаем
 
-            if not first_paragraph:
-                logger.warning(f"❌ Описание не найдено в `RESTRICT-TEXT`! {url}")
-                return {url: "Нет описания"}
+                try:
+                    # Ждём появления блока описания
+                    description_block = WebDriverWait(driver, 20).until(
+                        EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-test='OBJECT-DESCRIPTION-CONTENT']"))
+                    )
+                    soup = BeautifulSoup(description_block.get_attribute("innerHTML"), "html.parser")
 
-            # Берём текст внутри этого контейнера
-            description = first_paragraph.text.strip()
+                    # Пробуем найти основной текст внутри RESTRICT-TEXT
+                    first_paragraph = soup.find("div", {"data-test": "RESTRICT-TEXT"})
 
-            logger.info(f"✅ Описание найдено: {description}")
+                    if first_paragraph:
+                        new_description = first_paragraph.text.strip()
+                        logger.info(f"[{process_id}] [INFO] Описание: {new_description}")
 
-        except Exception as e:
-            logger.error(f"❌ Ошибка при обработке {url}: {e}")
+                        if len(description) > 5:
+                            descriptions[url] = new_description
+                        else:
+                            logger.info(f"[{process_id}] [INFO] Обнаруженное описание менее 5 символов. Установлено 'Нет описания'")
 
+                    break
+
+                except Exception as e:
+                    attempts += 1
+                    logger.error(f"[{process_id}] [ERROR {attempts}/{max_attempts}] Ошибка при обработке {url}: {e}")
+
+                    if attempts > 3:
+                        asyncio.run(delete_event_by_url(url))
+                        logger.warning(f"[{process_id}] [WARNING] Страница {url} не загрузилась! Удаляем из базы.")
+                        break
+
+                    driver.quit()
+                    time.sleep(5)
+                    driver = init_driver()
+                    logger.info(f'[{process_id}] [INFO] Браузер перезапущен')
+                    time.sleep(5)
+
+            time.sleep(random.uniform(0.5, 2))  # Задержка для избежания бана
+            current_count += 1
+        logger.info(f'[{process_id}] [INFO] возвращение значений descriptions')
+        return descriptions
+
+    except Exception as e:
+        logger.error(f"[{process_id}] [ERROR] Произошла ошибка:")
+        logger.error(traceback.format_exc())
+        return descriptions
 
     finally:
-        driver.quit()
-    return {url: description}
+
+        if 'driver' in locals():
+            driver.quit()
+            logger.info(f"[{process_id}] [INFO] Браузер закрыт!")
 
 
-def get_descriptions_parallel(process_id, urls: List[str], num_processes: int = 2) -> Dict[str, str]:
+def get_descriptions_parallel(urls: List[str], num_processes: int = 2) -> Dict[str, str]:
     chunk_size = max(1, len(urls) // num_processes)  # Разбиваем список ссылок на части
     url_chunks = [urls[i:i + chunk_size] for i in range(0, len(urls), chunk_size)]
 
