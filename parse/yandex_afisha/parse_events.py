060b9b4c7b862d55f5f265a354a199f72e584026
w1nn3rpy <l33sa1337@gmail.com>
2025-03-18 09:45:38 +0400

    fix

--- parse/yandex_afisha/parse_events.py
+++ parse/yandex_afisha/parse_events.py
@@ -55,6 +55,12 @@ def init_driver():
     }
     options.add_experimental_option("prefs", prefs)
     options.add_argument("--blink-settings=imagesEnabled=false")  # Отключаем загрузку изображений
+    options.add_argument(
+        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
+
+    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-popup-blocking")
 
 
     driver = uc.Chrome(options=options,
