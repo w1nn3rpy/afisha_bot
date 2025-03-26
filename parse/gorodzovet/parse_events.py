8c5f789208513546b2a2f9630490d63012c950e2
w1nn3rpy <l33sa1337@gmail.com>
2025-03-26 09:27:57 +0400

    fix

--- parse/gorodzovet/parse_events.py
+++ parse/gorodzovet/parse_events.py
@@ -134,7 +134,7 @@ def get_all_events_gorodzovet(urls: List[str]) -> List[dict] | None:
                     logger.info(f'Title: {title}')
                     event_link = f"https://www.gorodzovet.ru{href_tag['data-link']}"
                     logger.info(f'Event link: {event_link}')
-                    for word in title:
+                    for word in title.split(" "):
                         print(word)
                         if word.lower() in str_categories:
                             category = str_categories[title]
